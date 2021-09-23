20feaae938702557379484e1f531e3b7a71a82a5
IvanPsurtcev <87025698+IvanPsurtcev@users.noreply.github.com>
2021-09-24 01:45:00 +0300

    v1.1

---
+++ scripts/deploy.js
@@ -0,0 +1,20 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log(`Deploying contracts with the account: ${deployer.address}`);
+
+  const balance = await deployer.getBalance();
+  console.log(`Account balance: ${balance.toString()}`);
+
+  const Lottery = await ethers.getContractFactory('Lottery');
+  const lottery = await Lottery.deploy();
+  console.log(`Lottery address: ${lottery.address}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
